275646e84552982663f809c1fcc734c145d64490
Lockey-J <zhijia21cn@163.com>
2019-01-16 02:12:28 +0800

    zfb

--- 森林小号浇水.js
+++ 森林小号浇水.js
@@ -1,12 +1,40 @@
-//function clickCenter(obj){
+//点击控件中间位置
     function clickCenter(obj){
         var rect = obj.bounds();
+        var sdkint=device.sdkInt;
+        if (sdkint<24){
+            return  Tap(rect.centerX(), rect.centerY());
+        }
+        else{
+            return click(rect.centerX(), rect.centerY());
+        }
+        
+      };
+      function Waters(){
+        var to="初始";
+        var OutWater=false;
+        var thread1=threads.start(function () { events.observeToast();
+            events.onToast(function(toast){
+                to=toast.getText();
+                if (to.indexOf("上限")>-1) {
+                    OutWater=true;
+                    thread1.interrupt();
 
-        return click(rect.centerX(), rect.centerY());
-      }
-         
-      threads.start(function () { events.observeToast();
-        events.onToast(function(toast){
-            to=toast.getText()
-        });
-    }) 
+                }
+            });
+        }) ;
+        threads.start(function () {while(thread1.isAlive()){
+            var selector=descContains("浇水").findOne(2000);
+            console.log(selector);
+            // log(OutWater)
+            // log(selector && !OutWater)
+            if(selector && !OutWater){
+
+                clickCenter(selector)
+            }
+            sleep(1500);
+        }
+    })
+    }
+      
+    Waters();
