5691bad8bd0672df88c501e00f4d940c4ce0512a
Lockey-J <zhijia21cn@163.com>
2019-01-23 18:02:00 +0800

    201901231801

--- 支付宝领保护罩.js
+++ 支付宝领保护罩.js
@@ -58,6 +58,10 @@ function GetWalkNum(){
             
             toast("步数不足2万，等待刷新……"+"\n\r当前步数："+bushu);
             ra.swipe(device.width*0.5,device.height*0.2,device.width*0.5,device.height*0.8)
+            descContains("我的行走").findOne(5000);
+            descContains("今日步数").findOne(5000);
+            descContains("运动换卡币").findOne(5000);
+            toast("刷新完毕")
             sleep(8000);
             Back();
             sleep(2000)
@@ -163,11 +167,83 @@ function EnterSP(){
     
 }
 
-unlock();
 
-sleep(3000)
-GetWalkNum();
-OpenMY();
-i=0;
-device.setBrightnessMode(1);
-ra.exit();
+//返回按钮，兼容root跟安卓7
+function backfun(){
+    var sdkint=device.sdkInt;
+    if (sdkint<24){
+        return  Back();
+    }
+    else{
+        return back();
+    }
+}
+
+function backtoIndex(){
+    var prepackage=currentPackage();
+    while(true){
+        backfun();
+        sleep(500)
+        if(prepackage!=currentPackage()){       
+            break;               
+        }
+
+    }
+    sleep(3000);
+    ActionZFB();
+ 
+}
+function ActionZFB(){
+    launch("com.eg.android.AlipayGphone");
+    while(true){
+        var mpage=currentPackage();
+        if(mpage=="com.eg.android.AlipayGphone"){
+        
+            break;
+        }
+        else{
+            sleep(1000);
+        }
+    } 
+}
+try {
+    log(descContains("立即捐步").findOne(3000).clickable())
+} catch (error) {
+    log("出错")
+}
+
+// unlock();
+// sleep(3000)
+// GetWalkNum();
+// OpenMY();
+// i=0;
+// device.setBrightnessMode(1);
+// ra.exit();
+// backtoIndex();
+
+//报名行走积分赛
+function JFSBM(){
+    app.startActivity({        
+        action: "VIEW",
+        data: "alipays://platformapi/startapp?appId=68687129"    
+    });
+    toast("进入行走积分赛")
+    while( !descContains("行走积分赛").exists()){
+        sleep(300);
+    }   
+    while(descContains("加载中").exists()){
+        sleep(300);
+    } 
+    descContains("达标人数").findOne(8000)
+    if(descContains("已报名").exists()){
+        log("已报名")
+        return;
+    }
+    descContains("报名明日比赛").clickable().findOne(3000).click()
+    descContains("立即使用积分报名").findOne(3000)
+    descContains("行走积分赛报名").findOne(3000)
+    descContains("报名参赛").findOne(3000)
+    descContains("立即使用积分报名").clickable().findOne(3000).click()
+    textContains("确认报名").idContains("ensure").findOne(3000)
+    textContains("确认报名").idContains("ensure").findOne(3000).click()
+}
